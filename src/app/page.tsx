14fb5468b6ddb203e13ee6fe156535ae76c560d9
cykoravish <ravishbisht85@gmail.com>
2026-01-07 21:18:41 +0530

    update landing page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ export default function Home() {
   return (
    <main className="min-h-screen bg-gray-800 flex items-center justify-center px-6">
       <div className="max-w-4xl text-center text-white">
-        <h1 className="text-4xl md:text-6xl font-extrabold mb-6 tracking-tight">
+        <h1 className="text-4xl md:text-6xl font-extrabold mb-6 tracking-tight">  
           Web Dev Practice LMS 🚀
         </h1>
 
         <p className="text-lg md:text-xl opacity-90 mb-10">
-          Learn web development by building real projects, solving challenges,
-          and mastering HTML, CSS, JavaScript, React, and more.
+        Learn JavaScript by building real-world projects, solving coding challenges,
+and mastering core JS concepts, ES6+, DOM, and modern frameworks.
+
         </p>
 
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
